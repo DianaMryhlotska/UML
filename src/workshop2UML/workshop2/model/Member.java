367a2ada93556d54a9bab298fe1917824cf41b02
Diana Mryhlotska <diana.mryhlotska.kn.2016@lpnu.ua>
2019-09-30 13:09:26 +0300

    fkdfkd

--- src/workshop2UML/workshop2/model/Member.java
+++ src/workshop2UML/workshop2/model/Member.java
@@ -1,6 +1,8 @@
 package workshop2UML.workshop2.model;
 
+import java.util.Scanner;
 import java.util.LinkedList;
+import java.util.Random;
 
 
 public class Member {
@@ -37,20 +39,38 @@ public class Member {
         System.out.println("" + boat);
     }
 
+    public void createNewMember(String name){
+        Random rand = new Random();
+        this.setPersonalNumber(rand.nextInt(99999));
+        this.setName(name);
+    }
+
+    public void deleteMember(){}
+
+    public void changeInfoMember(){}
+
+    public void lookSpecInfo(Member member){
+        int choise;
+        Scanner in = new Scanner(System.in);
+        System.out.println("What do you want to look? Press 1 - Name, press 2 - PersonalID, press 3 - list of boats");
+
+        if()
+    }
+
+    public void registerBoat(Boat boat){
+        this.m_boat.add(boat);
+    }
+
+    public void deleteBoat(Boat boat){
+        this.m_boat.remove(boat);
+    }
+
+
     public void showInfo(){
         System.out.println("Name" + this.Name + "Boats:" + this.m_boat);
     }
 
-    /*
-    public LinkedList<Boat> getM_boat() {
-        return m_boat;
-    }
 
-    public void setM_boat(LinkedList<Boat> m_boat) {
-        this.m_boat = m_boat;
-    }
-
-    */
 
 
 }
