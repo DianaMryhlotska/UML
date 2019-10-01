bedbb948af82fb08f83d541c8adc6dd2dedb8989
Simon Crouzet <simon.crouzet@laposte.net>
2019-10-01 16:19:03 +0200

    Improves controller (on progress)

--- src/workshop2UML/workshop2/controller/User.java
+++ src/workshop2UML/workshop2/controller/User.java
@@ -1,13 +1,44 @@
 package workshop2UML.workshop2.controller;
 
+import workshop2UML.workshop2.model.Register;
 import workshop2UML.workshop2.view.Console;
 
 // TODO : Controls interactions between Models and View !
 // TODO : Review all !
 
 public class User {
+    private Console console;
+    private Register register;
 
-    public boolean Start(Console a_view, ClubSystem a_session) {
+    public User(Console console, Register register) {
+        this.console = console;
+        this.register = register;
+    }
+
+    public boolean startSystem() {
+        int choice=console.printMenu();
+
+        switch (choice) {
+            case 1: listMember();
+            case 2:;
+            case 3:;
+            case 4:;
+            case 5:;
+            case 6:;
+            case 7:;
+            case 8:;
+        }
+    }
+
+    public void listMember() {
+        int choice = console.printListMenu();
+        if (choice==1)
+            console.printCompactList(register.membersList());
+        else
+            console.printVerboseList(register.membersList());
+    }
+
+    /*public boolean Start(Console a_view, ClubSystem a_session) {
 
         a_view.collectEvents();
 
@@ -20,5 +51,5 @@ public class User {
         }
 
         return true;
-    }
+    }*/
 }
